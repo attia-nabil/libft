38b24af9c81a9a2579093082470134e3181e4d59
attia-nabil <attia.nabil@hotmail.com>
2021-11-29 21:25:52 +0100

    Push

--- libft.h
+++ libft.h
@@ -51,5 +51,6 @@ char *ft_itoa(int n);
 char *ft_strtrim(char const *s1, char const *set);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void ft_putnbr_fd(int n, int fd);
+void ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 #endif
